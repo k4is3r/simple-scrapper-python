bf129d75512ec529592d6141e63b9be074d10bc6
Eduardo Imery <ejwimery@gmail.com>
2019-12-02 09:14:42 -0600

    fixing some errors

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import requests
-from bs4 import Beautifulsoup
+from bs4 import BeautifulSoup
 
 
 page = requests.get('https://www.transparencia.gob.sv/institutions/capres/documents/otros-documentos-normativos?status=301')
