d7d858407c5fc11c833accfefd97fe20560b5a7c
Eduardo Imery <ejwimery@gmail.com>
2019-12-02 09:01:37 -0600

    requesting a page and extracting the content

--- main.py
+++ main.py
@@ -3,3 +3,6 @@ import requests
 from bs4 import Beautifulsoup
 
 
+page = requests.get('https://www.transparencia.gob.sv/institutions/capres/documents/otros-documentos-normativos?status=301')
+
+soup = BeautifulSoup(page.content, 'html.parser')
